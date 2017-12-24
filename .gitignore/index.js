ebd98cc682de4ca9dee5d4008cf029179df32a9c
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-24 17:20:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,9 +1,10 @@
 const Discord = require(discord.js");
-
+                        
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("Jinie, !help");
+    console.log("le bot a bien ete connecte");
 });
 
-bot.login("
+bot.login("Mzk0NDc2MjQzOTc5OTkzMDg5.DSFlDQ.vAj7kkqzqaWt9cy9QIive1dht9E");
