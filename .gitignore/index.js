ccffc69d48b209721bfa5f8ce85cda86d06cb224
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-26 18:11:05 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -58,10 +58,10 @@ __bonjour__
     else if(message.content === '!H0001') {
       const embed = new Discord.RichEmbed();
       embed.setTitle('Mon titre - 256 caractères')
-      .setAuthor('Nom', 'https://i.imgur.com/lm8s41J.png')
+      .setAuthor('', '')
       .setColor(3447003)
       .setDescription('```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. Un barde monte sur une table et commence à jouer une petite chanson… Malheureusement sa chanson est terrible et nique l’ambiance du bar.```')
-      .setFooter('Pied de page - 2048','http://i.imgur.com/w1vhFSR.png')
+      .setFooter('','')
       .setImage('http://i.imgur.com/yVpYmuV.png')
       .setThumbnail('https://i.imgur.com/p2qNFag.png')
       .setTimestamp()
