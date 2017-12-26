16b0408994a8bc60bf266dbab3d0d4063c18fac0
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-26 18:04:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -55,10 +55,10 @@ __bonjour__
         }
       }});
     }
-    else if(message.content === '!richEmbed3') {
+    else if(message.content === '!H0001') {
       const embed = new Discord.RichEmbed();
-      embed.setTitle('Rencontre a la Taverne', 'https://i.imgur.com/lm8s41J.png')
-      .setAuthor('Rencontre a la Taverne', 'https://i.imgur.com/lm8s41J.png')
+      embed.setTitle('Mon titre - 256 caractères')
+      .setAuthor('Nom', 'https://i.imgur.com/lm8s41J.png')
       .setColor(3447003)
       .setDescription('```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. Un barde monte sur une table et commence à jouer une petite chanson… Malheureusement sa chanson est terrible et nique l’ambiance du bar.```')
       .setFooter('Pied de page - 2048','http://i.imgur.com/w1vhFSR.png')
@@ -66,10 +66,11 @@ __bonjour__
       .setThumbnail('https://i.imgur.com/p2qNFag.png')
       .setTimestamp()
       .setURL('http://google.com');
-
-      embed.addBlankField(true)
-      .addField('Tu décides de faire quelque chose à ce sujet !', '-Le **Convaincre de Partir **')
-      .addField('', '-Le **Provoquer en Duel**');
+      
+      embed.addField('Markdown', 'Le **Markdown**, c\'est la *vie* ! ***Discord***')
+      .addField('Markdown', 'Le **Markdown**, c\'est la *vie* ! ***Discord***')
+      .addBlankField(true)
+      .addField('Markdown', 'Le **Markdown**, c\'est la *vie* ! ***Discord***');
       
       message.channel.send({embed: embed});
     }
