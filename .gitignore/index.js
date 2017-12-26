6e6df32f803e50677c7334f114e74044589a231b
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-26 18:35:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -57,7 +57,7 @@ __bonjour__
     }
     else if(message.content === '!H0001') {
       const embed = new Discord.RichEmbed();
-      embed.setTitle('```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. Un barde monte sur une table et commence à jouer une petite chanson… Malheureusement sa chanson est terrible et nique l’ambiance du bar.```')
+      embed.setTitle('test')
       .setAuthor('', '')
       .setColor(3066993)
       .setDescription('```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. Un barde monte sur une table et commence à jouer une petite chanson… Malheureusement sa chanson est terrible et nique l’ambiance du bar.```')
@@ -70,6 +70,13 @@ __bonjour__
       .addField('Tu décides de faire quelque chose à ce sujet !', 'Le Convaincre de Partir');
       
       message.channel.send({embed: embed});
+    }
+        }
+    else if(message.content === '!H0002') {
+      message.channel.send({embed: {
+        color: 3066993,
+        description: '```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. Un barde monte sur une table et commence à jouer une petite chanson… Malheureusement sa chanson est terrible et nique l’ambiance du bar.```'
+      }});
     }
   }
 });
