135a1e4a9af096afa764cb476ee3fae4541d9653
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-27 15:08:47 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -59,7 +59,6 @@ __bonjour__
       const embed = new Discord.RichEmbed();
       embed.setColor(3066993)
       .setDescription('```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. \n\nUn barde monte sur une table et commence à jouer une petite chanson… \n\nMalheureusement sa chanson est terrible et nique l’ambiance du bar.```')
-      .setFooter('Pied de page - 2048','http://i.imgur.com/w1vhFSR.png')
       .setImage('http://i.imgur.com/yVpYmuV.png')
       .setThumbnail('https://img15.hostingpics.net/pics/691096aqwzsxe.png')
       .setTimestamp()
