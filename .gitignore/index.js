b31a7a379c1d68d2df6f61d98d7895f5dda42429
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-27 17:32:48 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,7 +5,7 @@ var bot = new Discord.Client();
 const PREFIX = "!";
 
 bot.on('ready', function() {
-    bot.user.setGame("Race : Orc");
+    bot.user.setGame("Humain");
     console.log("le bot a bien ete connecte");
 });
 
@@ -55,18 +55,30 @@ __bonjour__
         }
       }});
     }
-    else if(message.content === '!richEmbed3') {
+    else if(message.content === '!Q040') {
       const embed = new Discord.RichEmbed();
       embed.setColor(3066993)
       .setDescription('```\nAlors que vous passez de longues heures accoudées au comptoir de la taverne de Marbrume a siroté une biere. \n\nUn barde monte sur une table et commence à jouer une petite chanson… \n\nMalheureusement sa chanson est terrible et nique l’ambiance du bar.```')
       .setThumbnail('https://img15.hostingpics.net/pics/166381aqwzsxe.png');
       
-      embed.addField('Le Convaincre de Partir', '!0044')
-      .addField('Le Provoquer en Duel', '!0045');
+      embed.addField('Le Convaincre de Partir', '!V041')
+      .addField('Le Provoquer en Duel', '!V042');
     
       message.channel.send({embed: embed});
     }
   }
 });
-
+    }
+    else if(message.content === '!V042') {
+      const embed = new Discord.RichEmbed();
+      embed.setColor(3066993)
+      .setDescription('```\nTu saisie la lutte du propriétaire et tu le provoque dans un duel musical ! \n\Tu commences juste à jouer de la lutte quand tu réalises que t’es encore pire que le barde… \n\Le Barde te sur joue facilement et te fait signe de baisser ta lutte… \n\Le Bar entier se fout de ta gueule… c’est très embarrassant…. \n\nUn barde monte sur une table et commence à jouer une petite chanson… \n\nMalheureusement sa chanson est terrible et nique l’ambiance du bar.```')
+      .setThumbnail('https://img15.hostingpics.net/pics/166381aqwzsxe.png');
+      
+      embed.addField('-5 en charisme', '***Tu fera mieux la prochaine fois***');
+    
+      message.channel.send({embed: embed});
+    }
+  }
+});
 bot.login("Mzk1NjA4NjQ2MzI0ODQ2NTky.DSVW3A.Zk2nKQZNoykSUqlVQxPcnZ91cmQ");
