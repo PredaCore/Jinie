3a4b683ec7e6f5153fda049d62ce09150b9bf87c
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-27 13:57:47 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,12 @@ bot.on('ready', function() {
     bot.user.setGame("Race : Humain");
     console.log("le bot a bien ete connecte");
 });
+bot.on('message', function(message) {
 
+        if(message.content === 'wolfayeur') {
+            message.reply('<3')
+        }
+    });
 bot.on('message', message => {
   if(message.content[0] === PREFIX) {
     if(message.content === '!markdown') {
